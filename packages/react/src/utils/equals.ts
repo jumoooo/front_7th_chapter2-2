4399d0a3ac574c980ffecb67cd6d2b61cba40f40
준모 김 <rnsdlsdmltk@gmail.com>
2025-11-16 23:43:27 +0900

    shallowEquals 작성 완료

--- packages/react/src/utils/equals.ts
+++ packages/react/src/utils/equals.ts
@@ -5,7 +5,35 @@
 export const shallowEquals = (a: unknown, b: unknown): boolean => {
   // 여기를 구현하세요.
   // Object.is(), Array.isArray(), Object.keys() 등을 활용하여 1단계 깊이의 비교를 구현합니다.
-  return a === b;
+
+  if (Object.is(a, b)) return true;
+  if (a === null || b === null) return false;
+  if (typeof a !== typeof b) return false;
+
+  // number
+  if (Array.isArray(a) && Array.isArray(b)) {
+    if (a.length !== b.length) return false;
+    for (let i = 0; i < a.length; i++) {
+      if (!Object.is(a[i], b[i])) return false;
+    }
+    return true;
+  }
+
+  if (typeof a === "object" && typeof b === "object") {
+    const objA = a as Record<string, unknown>;
+    const objB = b as Record<string, unknown>;
+
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) return false;
+    for (const key of keysA) {
+      if (!Object.prototype.hasOwnProperty.call(b, key)) return false;
+      if (!Object.is(objA[key], objB[key])) return false;
+    }
+    return true;
+  }
+  return Object.is(a, b);
 };
 
 /**
